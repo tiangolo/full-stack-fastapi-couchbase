ecd491e9089e86df1845384b655ed4870c3ba676
Sebastián Ramírez <tiangolo@gmail.com>
2018-09-26 04:10:28 +0400

    :fire: Remove obsolete DB code

--- {{cookiecutter.project_slug}}/backend/app/app/db/base.py
+++
@@ -1,6 +0,0 @@
-
-# Import all the models, so that Base has them before being
-# imported by Alembic or used by Flask
-from app.db.base_class import Base  # noqa
-from app.models.user import User  # noqa
-from app.models.role import Role  # noqa
