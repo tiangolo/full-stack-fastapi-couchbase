f428faab8aaf7ede60b7ac8551080b81fb914866
Sebastián Ramírez <tiangolo@gmail.com>
2018-09-26 04:07:15 +0400

    :recycle: Use utils for JWT user extraction

--- {{cookiecutter.project_slug}}/backend/app/app/core/jwt.py
+++ {{cookiecutter.project_slug}}/backend/app/app/core/jwt.py
@@ -6,7 +6,6 @@ from flask_jwt_extended import JWTManager
 # Import app code
 from ..main import app
 from app.db.utils import get_user
-from app.db.flask_session import db_session
 
 # Setup the Flask-JWT-Extended extension
 jwt = JWTManager(app)
@@ -14,4 +13,4 @@ jwt = JWTManager(app)
 
 @jwt.user_loader_callback_loader
 def get_current_user(identity):
-    return get_user(identity, db_session)
+    return get_user(identity)
