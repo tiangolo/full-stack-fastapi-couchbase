aba8d347703332e3dcebba983d72596352746f4b
Sebastián Ramírez <tiangolo@gmail.com>
2018-09-26 04:01:40 +0400

    :card_file_box: Update Schemas for CouchDB

--- {{cookiecutter.project_slug}}/backend/app/app/schemas/user.py
+++ {{cookiecutter.project_slug}}/backend/app/app/schemas/user.py
@@ -7,16 +7,12 @@ from marshmallow import fields
 
 # Import app code
 from .base import BaseSchema
-from .role import RoleSchema
 
 
 class UserSchema(BaseSchema):
     # Own properties
-    id = fields.Int()
-    created_at = fields.DateTime()
-    first_name = fields.Str()
-    last_name = fields.Str()
-    email = fields.Email()
-    is_active = fields.Bool()
-    is_superuser = fields.Bool()
-    roles = fields.Nested(RoleSchema, only=("id", "name"), many=True)
+    _id = fields.Str()
+    _rev = fields.Str()
+    name = fields.Str()
+    type = fields.Str()
+    roles = fields.List(fields.Str())
