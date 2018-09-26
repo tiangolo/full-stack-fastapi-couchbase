2c53e20191604cef63930ccf0391e3aa624848fc
Sebastián Ramírez <tiangolo@gmail.com>
2018-09-26 04:04:14 +0400

    :card_file_box: Upgrade init DB for CouchDB specific set up

--- {{cookiecutter.project_slug}}/backend/app/app/db/init_db.py
+++ {{cookiecutter.project_slug}}/backend/app/app/db/init_db.py
@@ -1,23 +1,34 @@
 from app.core import config
-from app.core.security import pwd_context
 
-from app.db.utils import get_role_by_name, create_role, get_user_by_username, create_user, assign_role_to_user
-from app.core.security import get_password_hash
+from app.db.utils import create_or_get_user, create_user_with_default_db
+from app.db.roles import add_user_to_db_admins, add_user_to_db_members
 
-from app.models.user import User
-from app.models.role import Role
+from app.db.database import (
+    get_client,
+    get_db_app,
+    get_db_users,
+    enable_cors,
+    setup_cookie,
+)
 
-def init_db(db_session):
-    # Tables should be created with Alembic migrations
-    # But if you don't want to use migrations, create
-    # the tables uncommenting the next line
-    # Base.metadata.create_all(bind=engine)
-
-    role = get_role_by_name("default", db_session)
-    if not role:
-        role = create_role("default", db_session)
-
-    user = get_user_by_username(config.FIRST_SUPERUSER, db_session)
-    if not user:
-        user = create_user(db_session, config.FIRST_SUPERUSER, config.FIRST_SUPERUSER_PASSWORD, is_superuser=True)
-        assign_role_to_user(role, user, db_session)
+def init_db():
+    # Secure main DB access by adding a single dummy user 'app'
+    client = get_client()
+    db_app = get_db_app(client)
+    add_user_to_db_admins("app", db_app)
+    add_user_to_db_members("app", db_app)
+    # Create first superuser
+    db_users = get_db_users(client)
+    create_or_get_user(
+        config.FIRST_SUPERUSER,
+        config.FIRST_SUPERUSER_PASSWORD,
+        is_superuser=True,
+        db_users=db_users,
+        client=client,
+    )
+    create_user_with_default_db(config.FIRST_SUPERUSER, config.FIRST_SUPERUSER_PASSWORD)
+    db_app.create_query_index(fields=["type", "username"])
+    db_users.create_query_index(fields=["type"])
+    enable_cors()
+    setup_cookie()
+    # enable_couch_peruser()
