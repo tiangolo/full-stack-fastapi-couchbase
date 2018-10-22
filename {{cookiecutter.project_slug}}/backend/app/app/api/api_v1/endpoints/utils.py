fa9bc570d8f2d2d2f7881e037f0c6c601319ef2c
Sebastián Ramírez <tiangolo@gmail.com>
2018-10-22 16:39:22 +0400

    :lock: Fix security in Celery test endpoint
    to only allow superuser

--- {{cookiecutter.project_slug}}/backend/app/app/api/api_v1/endpoints/utils.py
+++ {{cookiecutter.project_slug}}/backend/app/app/api/api_v1/endpoints/utils.py
@@ -4,13 +4,14 @@
 # Import installed packages
 from webargs import fields
 from flask_apispec import doc, use_kwargs, marshal_with
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import get_current_user, jwt_required
 
 # Import app code
 from app.main import app
 from app.api.api_v1.api_docs import docs, security_params
 from app.core import config
 from app.core.celery_app import celery_app
+from app.crud.user import check_if_user_is_superuser
 
 # Import Schemas
 from app.schemas.msg import MsgSchema
@@ -25,5 +26,10 @@ from app.schemas.msg import MsgSchema
 @marshal_with(MsgSchema())
 @jwt_required
 def route_test_celery(word):
+    current_user = get_current_user()  # type: User
+    if not current_user:
+        abort(400, "Could not authenticate user with provided token")
+    elif not check_if_user_is_superuser(current_user):
+        abort(400, "Not a superuser")
     celery_app.send_task("app.worker.test_celery", args=[word])
     return ({"msg": "Word received"}, 201)
