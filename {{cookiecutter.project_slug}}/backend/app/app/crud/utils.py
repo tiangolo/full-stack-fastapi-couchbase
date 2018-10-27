0d35185e60d2eb9e635116d22ef54cc56e8578bd
Sebastián Ramírez <tiangolo@gmail.com>
2018-10-27 18:41:08 +0400

    :art: Add util to ensure lists of strs from enums

--- {{cookiecutter.project_slug}}/backend/app/app/crud/utils.py
+++ {{cookiecutter.project_slug}}/backend/app/app/crud/utils.py
@@ -3,6 +3,19 @@ from couchbase.n1ql import N1QLQuery, N1QLRequest
 
 from app.core.config import COUCHBASE_BUCKET_NAME
 
+from typing import Sequence, Union
+from enum import Enum
+
+
+def ensure_enums_to_strs(items: Sequence[Union[Enum, str]]):
+    str_items = []
+    for item in items:
+        if isinstance(item, Enum):
+            str_items.append(str(item.value))
+        else:
+            str_items.append(str(item))
+    return str_items
+
 
 def get_all_documents_by_type(bucket: Bucket, *, doc_type: str, skip=0, limit=100):
     query_str = f"SELECT *, META().id as id FROM {COUCHBASE_BUCKET_NAME} WHERE type = $type LIMIT $limit OFFSET $skip;"
