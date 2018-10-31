1b282c4c0667e0e27dd68236509bb8379bd4f81b
Sebastián Ramírez <tiangolo@gmail.com>
2018-10-31 23:02:58 +0400

    :sparkles: Update all main layout with new menu sections

---
+++ {{cookiecutter.project_slug}}/frontend/src/views/main/Main.vue
@@ -0,0 +1,176 @@
+<template>
+  <div>
+    <v-navigation-drawer persistent :mini-variant="$store.state.dashboardMiniDrawer" v-model="showDrawer" fixed app>
+      <v-layout column fill-height>
+        <v-list>
+          <v-subheader>Main menu</v-subheader>
+          <v-list-tile to="/main/dashboard">
+            <v-list-tile-action>
+              <v-icon>web</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Dashboard</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile to="/main/profile/view">
+            <v-list-tile-action>
+              <v-icon>person</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Profile</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile to="/main/profile/edit">
+            <v-list-tile-action>
+              <v-icon>edit</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Edit Profile</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile to="/main/profile/password">
+            <v-list-tile-action>
+              <v-icon>vpn_key</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Change Password</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+        <v-divider></v-divider>
+        <v-list subheader v-show="$store.getters.hasAdminAccess">
+          <v-subheader>Admin</v-subheader>
+          <v-list-tile to="/main/admin/users/all">
+            <v-list-tile-action>
+              <v-icon>group</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Manage Users</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile to="/main/admin/users/create">
+            <v-list-tile-action>
+              <v-icon>person_add</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Create User</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+        <v-spacer></v-spacer>
+        <v-list>
+          <v-list-tile @click="logout">
+            <v-list-tile-action>
+              <v-icon>close</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Logout</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-divider></v-divider>
+          <v-list-tile @click="switchMiniDrawer">
+            <v-list-tile-action>
+              <v-icon v-html="$store.state.dashboardMiniDrawer ? 'chevron_right' : 'chevron_left'"></v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Collapse</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-layout>
+    </v-navigation-drawer>
+    <v-toolbar dark color="primary" app>
+      <v-toolbar-side-icon @click.stop="switchShowDrawer"></v-toolbar-side-icon>
+      <v-toolbar-title v-text="appName"></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-menu bottom left offset-y>
+        <v-btn slot="activator" icon>
+          <v-icon>more_vert</v-icon>
+        </v-btn>
+        <v-list>
+          <v-list-tile to="/main/profile">
+            <v-list-tile-content>
+              <v-list-tile-title>Profile</v-list-tile-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-icon>person</v-icon>
+            </v-list-tile-action>
+          </v-list-tile>
+          <v-list-tile @click="logout">
+            <v-list-tile-content>
+              <v-list-tile-title>Logout</v-list-tile-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-icon>close</v-icon>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+    </v-toolbar>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+    <v-footer class="pa-3" fixed app>
+      <v-spacer></v-spacer>
+      <span>&copy; {{appName}}</span>
+    </v-footer>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+import { appName } from "@/env";
+import {
+  setDashboardMiniDrawer,
+  setDashboardShowDrawer,
+  actionLogOut
+} from "@/store/constants";
+
+const routeGuardMain = async (to, from, next) => {
+  if (to.path === "/main") {
+    next("/main/dashboard");
+  } else {
+    next();
+  }
+};
+
+@Component
+export default class Main extends Vue {
+  public appName = appName;
+
+  public beforeRouteEnter(to, from, next) {
+    routeGuardMain(to, from, next);
+  }
+
+  public beforeRouteUpdate(to, from, next) {
+    routeGuardMain(to, from, next);
+  }
+
+  get showDrawer() {
+    return this.$store.state.dashboardShowDrawer;
+  }
+
+  set showDrawer(value) {
+    this.$store.commit(setDashboardShowDrawer, value);
+  }
+
+  public switchShowDrawer() {
+    this.$store.commit(
+      setDashboardShowDrawer,
+      !this.$store.state.dashboardShowDrawer
+    );
+  }
+
+  public switchMiniDrawer() {
+    this.$store.commit(
+      setDashboardMiniDrawer,
+      !this.$store.state.dashboardMiniDrawer
+    );
+  }
+
+  public logout() {
+    this.$store.dispatch(actionLogOut);
+  }
+}
+</script>
