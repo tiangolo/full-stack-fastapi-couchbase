5561a51e789d044046d70e85216fe889e8406608
Sebastián Ramírez <tiangolo@gmail.com>
2018-10-31 22:50:51 +0400

    :white_check_mark: Update user CRUD tests

--- {{cookiecutter.project_slug}}/backend/app/app/tests/crud/test_user.py
+++ {{cookiecutter.project_slug}}/backend/app/app/tests/crud/test_user.py
@@ -3,39 +3,32 @@ from app.tests.utils.utils import random_lower_string
 from app.db.bucket import bucket
 from app.crud.user import (
     upsert_user,
-    create_or_get_user,
     authenticate_user,
     check_if_user_is_active,
     check_if_user_is_superuser,
     get_user,
 )
 from app.models.role import RoleEnum
+from app.models.user import UserInCreate
 
 
 def test_create_user():
     email = random_lower_string()
     password = random_lower_string()
-    user = upsert_user(bucket, email, password)
+    user_in = UserInCreate(name=email, email=email, password=password)
+    user = upsert_user(bucket, user_in)
     assert hasattr(user, "name")
     assert user.name == email
-    assert hasattr(user, "password")
+    assert hasattr(user, "hashed_password")
     assert hasattr(user, "type")
     assert user.type == "userprofile"
-
-
-def test_not_create_user_if_exists():
-    email = random_lower_string()
-    password = random_lower_string()
-    user = create_or_get_user(bucket, email, password)
-    same_user = create_or_get_user(bucket, email, password)
-    assert user.Meta.key == same_user.Meta.key
-    assert user.json_dict() == same_user.json_dict()
 
 
 def test_authenticate_user():
     email = random_lower_string()
     password = random_lower_string()
-    user = create_or_get_user(bucket, email, password)  # noqa
+    user_in = UserInCreate(name=email, email=email, password=password)
+    user = upsert_user(bucket, user_in)
     authenticated_user = authenticate_user(bucket, email, password)
     assert authenticated_user
     assert user.Meta.key == authenticated_user.Meta.key
@@ -51,7 +44,8 @@ def test_not_authenticate_user():
 def test_check_if_user_is_active():
     email = random_lower_string()
     password = random_lower_string()
-    user = create_or_get_user(bucket, email, password)  # noqa
+    user_in = UserInCreate(name=email, email=email, password=password)
+    user = upsert_user(bucket, user_in)
     is_active = check_if_user_is_active(user)
     assert is_active is True
 
@@ -59,7 +53,8 @@ def test_check_if_user_is_active():
 def test_check_if_user_is_active_inactive():
     email = random_lower_string()
     password = random_lower_string()
-    user = create_or_get_user(bucket, email, password, disabled=True)
+    user_in = UserInCreate(name=email, email=email, password=password, disabled=True)
+    user = upsert_user(bucket, user_in)
     is_active = check_if_user_is_active(user)
     assert is_active is False
 
@@ -67,7 +62,8 @@ def test_check_if_user_is_active_inactive():
 def test_check_if_user_is_superuser():
     email = random_lower_string()
     password = random_lower_string()
-    user = create_or_get_user(bucket, email, password, admin_roles=[RoleEnum.superuser])
+    user_in = UserInCreate(name=email, email=email, password=password, admin_roles=[RoleEnum.superuser])
+    user = upsert_user(bucket, user_in)
     is_superuser = check_if_user_is_superuser(user)
     assert is_superuser is True
 
@@ -75,7 +71,8 @@ def test_check_if_user_is_superuser():
 def test_check_if_user_is_superuser_normal_user():
     username = random_lower_string()
     password = random_lower_string()
-    user = create_or_get_user(bucket, username, password)
+    user_in = UserInCreate(name=username, email=username, password=password)
+    user = upsert_user(bucket, user_in)
     is_superuser = check_if_user_is_superuser(user)
     assert is_superuser is False
 
@@ -83,8 +80,8 @@ def test_check_if_user_is_superuser_normal_user():
 def test_get_user():
     password = random_lower_string()
     username = random_lower_string()
-    user = create_or_get_user(bucket, username, password)
+    user_in = UserInCreate(name=username, email=username, password=password, admin_roles=[RoleEnum.superuser])
+    user = upsert_user(bucket, user_in)
     user_2 = get_user(bucket, username)
     assert user.Meta.key == user_2.Meta.key
     assert user.json_dict() == user_2.json_dict()
-
