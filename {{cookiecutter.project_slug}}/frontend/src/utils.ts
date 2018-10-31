cbc91ba43c55929c4e63952fcf0683286d238c76
Sebastián Ramírez <tiangolo@gmail.com>
2018-10-31 22:53:01 +0400

    :fire: Remove unnecessary utils, give more power to actions

--- {{cookiecutter.project_slug}}/frontend/src/utils.ts
+++ {{cookiecutter.project_slug}}/frontend/src/utils.ts
@@ -1,43 +1,5 @@
-import { Store } from 'vuex';
+export const getLocalToken = () => localStorage.getItem('token');
 
-import { State, setToken, setLoggedIn } from '@/store';
-import { api } from '@/api';
+export const saveLocalToken = (token: string) => localStorage.setItem('token', token);
 
-
-export const checkGetToken = (store: Store<State>) => {
-    if (store.state.token) {
-        return store.state.token;
-    } else {
-        const token = localStorage.getItem('token');
-        if (token) {
-            store.commit(setToken, token);
-        }
-        return token || '';
-    }
-};
-
-export const checkIsLoggedIn = async (store: Store<State>) => {
-    if (store.state.isLoggedIn) {
-        return true;
-    } else {
-        const token = checkGetToken(store);
-        if (token) {
-            const loggedIn = await api.getMe(token).then((response) => {
-                store.commit(setLoggedIn, true);
-                return true;
-            }).catch((err) => {
-                removeToken();
-                store.commit(setToken, '');
-                return false;
-            });
-            return loggedIn;
-        } else {
-            store.commit(setLoggedIn, false);
-            return false;
-        }
-    }
-};
-
-export const saveToken = (token: string) => localStorage.setItem('token', token);
-
-export const removeToken = () => localStorage.removeItem('token');
+export const removeLocalToken = () => localStorage.removeItem('token');
