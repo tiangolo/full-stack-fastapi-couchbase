da9a5ee4bbbf4534205bea76e0297452f2c5c937
Sebastián Ramírez <tiangolo@gmail.com>
2018-10-31 23:02:12 +0400

    :sparkles: Add a new simple Dashboard with main link-buttons
    as a less-lame first page

---
+++ {{cookiecutter.project_slug}}/frontend/src/views/main/Dashboard.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container fluid>
+    <v-card class="ma-3 pa-3">
+      <v-card-title primary-title>
+        <div class="headline primary--text">Dashboard</div>
+      </v-card-title>
+      <v-card-text>
+        <div class="headline font-weight-light ma-5">Welcome {{greetedUser}}</div>
+        <v-btn to="/main/profile/view">View Profile</v-btn>
+        <v-btn to="/main/profile/edit">Edit Profile</v-btn>
+        <v-btn to="/main/profile/password">Change Password</v-btn>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { Store } from "vuex";
+import { State } from "@/store";
+
+@Component
+export default class Dashboard extends Vue {
+  get greetedUser() {
+    if(this.$store.state.userProfile.human_name) {
+      return this.$store.state.userProfile.human_name;
+    } else {
+      return this.$store.state.userProfile.name;
+    }
+  }
+}
+</script>
