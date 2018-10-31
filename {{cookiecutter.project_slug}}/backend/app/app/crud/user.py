4aa8b3565ab34d4ccb23e6a44333faaba8d88c89
Sebastián Ramírez <tiangolo@gmail.com>
2018-10-31 22:48:16 +0400

    :building_construction: Update user CRUD operations and utils

--- {{cookiecutter.project_slug}}/backend/app/app/crud/user.py
+++ {{cookiecutter.project_slug}}/backend/app/app/crud/user.py
@@ -1,8 +1,8 @@
 # Standard module packages
 import logging
+
 # Installed packages
 from couchbase.bucket import Bucket
-from couchbase.n1ql import N1QLQuery
 import requests
 
 # Install app code
@@ -11,18 +11,16 @@ from app.core.config import (
     COUCHBASE_SYNC_GATEWAY_DATABASE,
     COUCHBASE_SYNC_GATEWAY_HOST,
     COUCHBASE_SYNC_GATEWAY_PORT,
-    COUCHBASE_SYNC_GATEWAY_USER,
-    COUCHBASE_SYNC_GATEWAY_PASSWORD,
 )
 from app.core.security import get_password_hash, verify_password
-from app.models.user import User
+from app.models.user import UserStored, UserSyncIn, UserInCreate, UserInUpdate
 from app.models.role import RoleEnum
 from app.models.config import USERPROFILE_DOC_TYPE
 from app.crud.utils import get_all_documents_by_type, ensure_enums_to_strs
 
 # Types
 from typing import Sequence, Mapping, List, Union  # noqa
-from enum import Enum
+
 
 def get_user_doc_id(name):
     return f"userprofile::{name}"
@@ -32,30 +30,18 @@ def get_user(bucket: Bucket, name: str):
     doc_id = get_user_doc_id(name)
     result = bucket.get(doc_id, quiet=True)
     if not result.value:
-        return result.value
-    user = User(**result.value)
+        return None
+    print(result.value)
+    user = UserStored(**result.value)
     user.Meta.key = result.key
     return user
 
 
-def upsert_sync_gateway_user(
-    name: str,
-    password: str,
-    *,
-    admin_channels: Sequence[Union[Enum, str]] = [],
-    admin_roles: Sequence[Union[RoleEnum, str]] = [],
-    disabled=False,
-    email: str = None,
-):
+def upsert_sync_gateway_user(user: UserSyncIn):
+    name = user.name
     url = f"http://{COUCHBASE_SYNC_GATEWAY_HOST}:{COUCHBASE_SYNC_GATEWAY_PORT}/{COUCHBASE_SYNC_GATEWAY_DATABASE}/_user/{name}"
-    data = {
-        "name": name,
-        "password": password,
-        "admin_channels": ensure_enums_to_strs(admin_channels),
-        "admin_roles": ensure_enums_to_strs(admin_roles),
-        "email": email,
-        "disabled": disabled,
-    }
+
+    data = user.json_dict()
     response = requests.put(url, json=data)
     print(response)
     print(response.text)
@@ -64,89 +50,59 @@ def upsert_sync_gateway_user(
     return response.status_code == 200 or response.status_code == 201
 
 
-def upsert_user_in_db(
-    bucket: Bucket,
-    name: str,
-    password: str,
-    *,
-    admin_channels: Sequence[Union[Enum, str]] = [],
-    admin_roles: Sequence[Union[RoleEnum, str]] = [],
-    disabled=False,
-    email: str = None,
-    human_name: str = None,
-):
-    user_doc_id = get_user_doc_id(name)
-    store_password = get_password_hash(password)
-    user = User(
-        name=name,
-        password=store_password,
-        human_name=human_name,
-        email=email,
-        admin_roles=ensure_enums_to_strs(admin_roles),
-        admin_channels=ensure_enums_to_strs(admin_channels),
-        disabled=disabled,
-    )
+def update_sync_gateway_user(user: UserSyncIn):
+    name = user.name
+    url = f"http://{COUCHBASE_SYNC_GATEWAY_HOST}:{COUCHBASE_SYNC_GATEWAY_PORT}/{COUCHBASE_SYNC_GATEWAY_DATABASE}/_user/{name}"
+    if user.password:
+        data = user.json_dict()
+    else:
+        data = user.json_dict(exclude=set(["password"]))
+    response = requests.put(url, json=data)
+    print(response)
+    print(response.text)
+    logging.info(response)
+    logging.info(response.text)
+    return response.status_code == 200 or response.status_code == 201
+
+
+def upsert_user_in_db(bucket: Bucket, user_in: UserInCreate):
+    user_doc_id = get_user_doc_id(user_in.name)
+    passwordhash = get_password_hash(user_in.password)
+
+    user = UserStored(**user_in.json_dict(), hashed_password=passwordhash)
     doc_data = user.json_dict()
     bucket.upsert(user_doc_id, doc_data)
-    user = get_user(bucket, name)
     return user
 
 
-def upsert_user(
-    bucket: Bucket,
-    name: str,
-    password: str,
-    *,
-    admin_channels: Sequence[Union[Enum, str]] = [],
-    admin_roles: Sequence[Union[RoleEnum, str]] = [],
-    disabled=False,
-    email: str = None,
-    human_name: str = None,
-):
-    user = upsert_user_in_db(
-        bucket,
-        name,
-        password,
-        admin_channels=admin_channels,
-        admin_roles=admin_roles,
-        disabled=disabled,
-        email=email,
-        human_name=human_name,
-    )
-    assert upsert_sync_gateway_user(
-        name,
-        password,
-        admin_channels=admin_channels,
-        admin_roles=admin_roles,
-        disabled=disabled,
-        email=email,
-    )
+def update_user_in_db(bucket: Bucket, user_in: UserInUpdate):
+    stored_user = get_user(bucket, user_in.name)
+    for field in stored_user.fields:
+        print('stored field', field)
+        if field in user_in.fields:
+            print('input field', field)
+            value_in = getattr(user_in, field)
+            if value_in is not None:
+                setattr(stored_user, field, value_in)
+    if user_in.password:
+        passwordhash = get_password_hash(user_in.password)
+        stored_user.hashed_password = passwordhash
+    data = stored_user.json_dict()
+    bucket.upsert(stored_user.Meta.key, data)
+    return stored_user
+
+
+def upsert_user(bucket: Bucket, user_in: UserInCreate):
+    user = upsert_user_in_db(bucket, user_in)
+    user_in_sync = UserSyncIn(**user_in.json_dict())
+    assert upsert_sync_gateway_user(user_in_sync)
     return user
 
-def create_or_get_user(
-    bucket: Bucket,
-    name: str,
-    password: str,
-    *,
-    admin_channels: Sequence[Union[Enum, str]] = [],
-    admin_roles: Sequence[Union[RoleEnum, str]] = [],
-    disabled=False,
-    email: str = None,
-    human_name: str = None,
-):
-    user = get_user(bucket, name)
-    if user:
-        return user
-    user = upsert_user(
-        bucket,
-        name,
-        password,
-        admin_channels=admin_channels,
-        admin_roles=admin_roles,
-        disabled=disabled,
-        email=email,
-        human_name=human_name,
-    )
+
+def update_user(bucket: Bucket, user_in: UserInUpdate):
+    user = update_user_in_db(bucket, user_in)
+    user_in_sync = UserSyncIn(**user_in.json_dict())
+    assert update_sync_gateway_user(user_in_sync)
     return user
 
 
@@ -154,17 +110,25 @@ def authenticate_user(bucket: Bucket, name: str, password: str):
     user = get_user(bucket, name)
     if not user:
         return False
-    if not verify_password(password, user.password):
+    if not verify_password(password, user.hashed_password):
         return False
     return user
 
 
-def check_if_user_is_active(user: User):
+def check_if_user_is_active(user: UserStored):
     return not user.disabled
 
 
-def check_if_user_is_superuser(user: User):
-    return RoleEnum.superuser.value in user.admin_roles
+def check_if_user_is_superuser(user: UserStored):
+    return RoleEnum.superuser.value in ensure_enums_to_strs(user.admin_roles)
+
+
+def check_if_user_is_admin(user: UserStored):
+    return RoleEnum.admin.value in ensure_enums_to_strs(user.admin_roles)
+
+
+def check_if_user_is_admin_or_superuser(user: UserStored):
+    return check_if_user_is_admin(user) or check_if_user_is_superuser(user)
 
 
 def get_users(bucket: Bucket, *, skip=0, limit=100):
@@ -175,7 +139,7 @@ def get_users(bucket: Bucket, *, skip=0, limit=100):
     for item in doc_results:
         data = item[COUCHBASE_BUCKET_NAME]
         doc_id = item["id"]
-        user = User(**data)
+        user = UserStored(**data)
         user.Meta.key = doc_id
         users.append(user)
     return users
