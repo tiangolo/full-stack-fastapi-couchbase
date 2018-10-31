c4bbcd7a72db9d3b4ab1f0359356d2b472c8998f
Sebastián Ramírez <tiangolo@gmail.com>
2018-10-31 22:54:22 +0400

    :art: Put constants in separated file
    and add new actions and mutations

---
+++ {{cookiecutter.project_slug}}/frontend/src/store/constants.ts
@@ -0,0 +1,31 @@
+// Mutations
+export const setToken = 'setToken';
+export const setLoggedIn = 'setLoggedIn';
+export const setUserProfile = 'setUserProfile';
+export const setLogInError = 'setLogInError';
+export const setDashboardMiniDrawer = 'setDashboardMiniDrawer';
+export const setDashboardShowDrawer = 'setDashboardShowDrawer';
+
+// Actions
+export const actionCheckLoggedIn = 'actionCheckLoggedIn';
+export const actionLogIn = 'actionLogIn';
+export const actionRemoveLogIn = 'actionRemoveLogIn';
+export const actionLogOut = 'actionLogOut';
+export const actionRouteLogOut = 'actionRouteLogOut';
+export const actionRouteLoggedIn = 'actionRouteLoggedIn';
+export const actionGetUserProfile = 'actionGetUserProfile';
+export const actionUpdateUserProfile = 'actionUpdateUserProfile';
+export const actionCheckApiError = 'actionCheckApiError';
+
+
+// Admin Mutations
+export const setUsers = 'setUsers';
+export const setUser = 'setUser';
+export const setRoles = 'setRoles';
+
+
+// Admin Actions
+export const actionGetUsers = 'actionGetUsers';
+export const actionUpdateUser = 'actionUpdateUser';
+export const actionCreateUser = 'actionCreateUser';
+export const actionGetRoles = 'actionGetRoles';
