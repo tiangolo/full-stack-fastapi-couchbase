7bdae42e103b20d3dc7fb1df6e04637308f977a9
Sebastián Ramírez <tiangolo@gmail.com>
2018-10-31 22:58:11 +0400

    :sparkles: Update frontend main init with Vee-validate
    and store in new location

--- {{cookiecutter.project_slug}}/frontend/src/main.ts
+++ {{cookiecutter.project_slug}}/frontend/src/main.ts
@@ -3,9 +3,10 @@ import '@babel/polyfill';
 import './component-hooks';
 import Vue from 'vue';
 import './plugins/vuetify';
+import './plugins/vee-validate';
 import App from './App.vue';
 import router from './router';
-import store from './store';
+import store from '@/store';
 import './registerServiceWorker';
 import 'vuetify/dist/vuetify.min.css';
 
