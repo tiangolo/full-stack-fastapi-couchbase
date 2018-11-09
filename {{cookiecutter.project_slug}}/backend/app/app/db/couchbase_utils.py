fa7e6403292fa44a45f89d7209bb397b86e1c12c
Sebastián Ramírez <tiangolo@gmail.com>
2018-11-09 10:31:54 +0400

    :bug: Update initial DB setup to use REST
    instead of provided module utils, as those create seg faults all the time

--- {{cookiecutter.project_slug}}/backend/app/app/db/couchbase_utils.py
+++ {{cookiecutter.project_slug}}/backend/app/app/db/couchbase_utils.py
@@ -3,6 +3,12 @@ import logging
 import requests
 from requests.auth import HTTPBasicAuth
 
+from app.core.config import (
+    COUCHBASE_MEMORY_QUOTA_MB,
+    COUCHBASE_INDEX_MEMORY_QUOTA_MB,
+    COUCHBASE_FTS_MEMORY_QUOTA_MB,
+)
+
 COUCHBASE_DEFAULT_USER = "Administrator"
 COUCHBASE_DEFAULT_PASSWORD = "password"  # noqa
 
@@ -33,56 +39,53 @@ def get_allowed_username(username):
     return modified_username
 
 
-def is_couchbase_ready(base_url):
-    r = requests.get(base_url)
+def is_couchbase_ready(cluster_url):
+    r = requests.get(cluster_url)
     return r.status_code == 200
 
 
-def setup_couchbase_services(base_url, username, password):
+def setup_couchbase_services(*, cluster_url, username, password):
     auth = HTTPBasicAuth(username, password)
-    url = f"{base_url}/node/controller/setupServices"
-    r = requests.post(
-        url,
-        data={"services": "kv,index,fts,n1ql", "setDefaultMemQuota": True},
-        auth=auth,
-    )
+    url = f"{cluster_url}/node/controller/setupServices"
+    r = requests.post(url, data={"services": "kv,index,fts,n1ql"}, auth=auth)
     return (
         r.status_code == 200
         or "cannot change node services after cluster is provisioned" in r.text
     )
 
 
-def is_default_bucket_setup(base_url, username, password):
-    url = f"{base_url}/pools/default/buckets"
+def setup_memory_quota(
+    *,
+    cluster_url,
+    username,
+    password,
+    memory_quota_mb="256",
+    index_memory_quota_mb="256",
+    fts_memory_quota_mb="256",
+):
     auth = HTTPBasicAuth(username, password)
-    r = requests.get(url, auth=auth)
-    return r.status_code == 200
-
-
-def setup_default_bucket(base_url, username, password, ram_quota_mb=597):
-    url = f"{base_url}/pools/default/buckets"
-    auth = HTTPBasicAuth(username, password)
+    url = f"{cluster_url}/pools/default"
     r = requests.post(
-        url, data={"name": "default", "ramQuotaMB": ram_quota_mb}, auth=auth
+        url,
+        data={
+            "memoryQuota": memory_quota_mb,
+            "indexMemoryQuota": index_memory_quota_mb,
+            "ftsMemoryQuota": fts_memory_quota_mb,
+        },
+        auth=auth,
     )
     return r.status_code == 200
 
 
-def setup_index_storage(base_url, username, password):
-    url = f"{base_url}/settings/indexes"
+def setup_index_storage(*, cluster_url, username, password):
+    url = f"{cluster_url}/settings/indexes"
     auth = HTTPBasicAuth(username, password)
     r = requests.post(url, data={"storageMode": "forestdb"}, auth=auth)
     return r.status_code == 200
 
 
-def ensure_default_bucket_setup(base_url, username, password, ram_quota_mb=597):
-    if is_default_bucket_setup(base_url, username, password):
-        return True
-    return setup_default_bucket(base_url, username, password, ram_quota_mb=ram_quota_mb)
-
-
-def setup_couchbase_username_password(base_url, username, password):
-    url = f"{base_url}/settings/web?just_validate=0"
+def setup_couchbase_username_password(*, cluster_url, username, password):
+    url = f"{cluster_url}/settings/web"
     auth = HTTPBasicAuth(COUCHBASE_DEFAULT_USER, COUCHBASE_DEFAULT_PASSWORD)
     r = requests.post(
         url,
@@ -92,21 +95,21 @@ def setup_couchbase_username_password(base_url, username, password):
     return r.status_code == 200
 
 
-def check_couchbase_username_password(base_url, username, password):
-    url = f"{base_url}/settings/web"
+def check_couchbase_username_password(*, cluster_url, username, password):
+    url = f"{cluster_url}/settings/web"
     auth = HTTPBasicAuth(username, password)
     r = requests.get(url, auth=auth)
     return r.status_code == 200
 
 
-def ensure_couchbase_username_password(base_url, username, password):
-    if setup_couchbase_username_password(base_url, username, password):
+def ensure_couchbase_username_password(*, cluster_url, username, password):
+    if setup_couchbase_username_password(cluster_url=cluster_url, username=username, password=password):
         return True
-    return check_couchbase_username_password(base_url, username, password)
+    return check_couchbase_username_password(cluster_url=cluster_url, username=username, password=password)
 
 
-def import_couchbase_default_data(base_url, username, password):
-    url = f"{base_url}/sampleBuckets/install"
+def import_couchbase_default_data(*, cluster_url, username, password):
+    url = f"{cluster_url}/sampleBuckets/install"
     auth = HTTPBasicAuth(username, password)
     r = requests.post(url, json=["travel-sample"], auth=auth)
     return (
@@ -115,35 +118,121 @@ def import_couchbase_default_data(base_url, username, password):
     )
 
 
-def config_couchbase(username, password, host="couchbase", port="8091"):
+def is_bucket_created(*, cluster_url, username, password, bucket_name):
+    url = f"{cluster_url}/pools/default/buckets/{bucket_name}"
+    auth = HTTPBasicAuth(username, password)
+    r = requests.get(url, auth=auth)
+    return r.status_code == 200
+
+
+def create_bucket(
+    *,
+    cluster_url,
+    username,
+    password,
+    bucket_name,
+    ram_quota_mb=100,
+    bucket_type="couchbase",
+):
+    url = f"{cluster_url}/pools/default/buckets"
+    auth = HTTPBasicAuth(username, password)
+    data = {"name": bucket_name, "ramQuotaMB": ram_quota_mb, "bucketType": bucket_type}
+    r = requests.post(url, data=data, auth=auth)
+    return r.status_code == 202
+
+
+def ensure_create_bucket(
+    *,
+    cluster_url,
+    username,
+    password,
+    bucket_name,
+    ram_quota_mb=100,
+    bucket_type="couchbase",
+):
+    if is_bucket_created(cluster_url=cluster_url, username=username, password=password, bucket_name=bucket_name):
+        return True
+    return create_bucket(
+        cluster_url=cluster_url,
+        username=username,
+        password=password,
+        bucket_name=bucket_name,
+        ram_quota_mb=ram_quota_mb,
+        bucket_type=bucket_type,
+    )
+
+
+def is_couchbase_user_created(*, cluster_url, username, password, new_user_id):
+    url = f"{cluster_url}/settings/rbac/users/local/{new_user_id}"
+    auth = HTTPBasicAuth(username, password)
+    r = requests.get(url, auth=auth)
+    return r.status_code == 200
+
+
+def create_couchbase_user(
+    *, cluster_url, username, password, new_user_id, new_user_password
+):
+    url = f"{cluster_url}/settings/rbac/users/local/{new_user_id}"
+    auth = HTTPBasicAuth(username, password)
+    data = {
+        "name": "",
+        "roles": "ro_admin,bucket_full_access[*]",
+        "password": new_user_password,
+    }
+    r = requests.put(url, data=data, auth=auth)
+    return r.status_code == 200
+
+
+def ensure_create_couchbase_user(*, cluster_url, username, password, new_user_id, new_user_password):
+    if is_couchbase_user_created(cluster_url=cluster_url, username=username, password=password, new_user_id=new_user_id):
+        return True
+    return create_couchbase_user(cluster_url=cluster_url, username=username, password=password, new_user_id=new_user_id, new_user_password=new_user_password)
+
+
+def config_couchbase(
+    *, username, password, enterprise=False, host="couchbase", port="8091"
+):
     allowed_username = get_allowed_username(username)
     assert (
         username == allowed_username
     ), f"Not a valid username: {username}, a valid one would be {allowed_username}"
     cluster_url = get_cluster_http_url(host=host, port=port)
-    logger.info("before is_sandbox_ready")
+    logger.info("before is_couchbase_ready")
     assert is_couchbase_ready(cluster_url)
-    logger.info("after is_sandbox_ready")
+    logger.info("after is_couchbase_ready")
 
-    logger.info("before setup_sandbox_services")
+    logger.info("before setup_couchbase_services")
     assert setup_couchbase_services(
-        cluster_url, COUCHBASE_DEFAULT_USER, COUCHBASE_DEFAULT_PASSWORD
-    ) or setup_couchbase_services(cluster_url, username, password)
-    logger.info("after setup_sandbox_services")
+        cluster_url=cluster_url, username=COUCHBASE_DEFAULT_USER, password=COUCHBASE_DEFAULT_PASSWORD
+    ) or setup_couchbase_services(cluster_url=cluster_url, username=username, password=password)
+    logger.info("after setup_couchbase_services")
 
-    logger.info("before ensure_default_bucket_setup")
-    assert ensure_default_bucket_setup(
-        cluster_url, COUCHBASE_DEFAULT_USER, COUCHBASE_DEFAULT_PASSWORD
-    ) or ensure_default_bucket_setup(cluster_url, username, password)
-    logger.info("after ensure_default_bucket_setup")
+    logger.info("before setup_memory_quota")
+    assert setup_memory_quota(
+        cluster_url=cluster_url,
+        username=COUCHBASE_DEFAULT_USER,
+        password=COUCHBASE_DEFAULT_PASSWORD,
+        memory_quota_mb=COUCHBASE_MEMORY_QUOTA_MB,
+        fts_memory_quota_mb=COUCHBASE_FTS_MEMORY_QUOTA_MB,
+        index_memory_quota_mb=COUCHBASE_INDEX_MEMORY_QUOTA_MB,
+    ) or setup_memory_quota(
+        cluster_url=cluster_url,
+        username=username,
+        password=password,
+        memory_quota_mb=COUCHBASE_MEMORY_QUOTA_MB,
+        fts_memory_quota_mb=COUCHBASE_FTS_MEMORY_QUOTA_MB,
+        index_memory_quota_mb=COUCHBASE_INDEX_MEMORY_QUOTA_MB,
+    )
+    logger.info("after setup_memory_quota")
 
-    logger.info("before setup_index_storage")
-    assert setup_index_storage(
-        cluster_url, COUCHBASE_DEFAULT_USER, COUCHBASE_DEFAULT_PASSWORD
-    ) or setup_index_storage(cluster_url, username, password)
-    logger.info("after setup_index_storage")
+    if not enterprise:
+        logger.info("before setup_index_storage")
+        assert setup_index_storage(
+            cluster_url=cluster_url, username=COUCHBASE_DEFAULT_USER, password=COUCHBASE_DEFAULT_PASSWORD
+        ) or setup_index_storage(cluster_url=cluster_url, username=username, password=password)
+        logger.info("after setup_index_storage")
 
-    logger.info("before ensure_sandbox_username_password")
-    assert ensure_couchbase_username_password(cluster_url, username, password)
-    logger.info("after ensure_sandbox_username_password")
+    logger.info("before ensure_couchbase_username_password")
+    assert ensure_couchbase_username_password(cluster_url=cluster_url, username=username, password=password)
+    logger.info("after ensure_couchbase_username_password")
     return True
