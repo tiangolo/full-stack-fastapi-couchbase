36f1c61bf31ae5c85f5aef588405db7a8e82c4f8
Sebastián Ramírez <tiangolo@gmail.com>
2018-11-20 16:48:42 +0400

    :sparkles: Add endpoint to test emails

--- {{cookiecutter.project_slug}}/backend/app/app/api/api_v1/endpoints/utils.py
+++ {{cookiecutter.project_slug}}/backend/app/app/api/api_v1/endpoints/utils.py
@@ -10,6 +10,7 @@ from app.api.api_v1.api_docs import docs, security_params
 from app.core import config
 from app.core.celery_app import celery_app
 from app.crud.user import check_if_user_is_superuser
+from app.utils import send_test_email
 
 # Import Schemas
 from app.schemas.msg import MsgSchema
@@ -31,3 +32,19 @@ def route_test_celery(word):
         abort(400, "Not a superuser")
     celery_app.send_task("app.worker.test_celery", args=[word])
     return ({"msg": "Word received"}, 201)
+
+
+@docs.register
+@doc(description="Test email", security=security_params, tags=["utils"])
+@app.route(f"{config.API_V1_STR}/test-email/", methods=["POST"])
+@use_kwargs({"email_to": fields.String(required=True)})
+@marshal_with(MsgSchema())
+@jwt_required
+def route_test_email(email_to):
+    current_user = get_current_user()  # type: User
+    if not current_user:
+        abort(400, "Could not authenticate user with provided token")
+    elif not check_if_user_is_superuser(current_user):
+        abort(400, "Not a superuser")
+    send_test_email(email_to=email_to)
+    return ({"msg": "Test email sent"}, 201)
