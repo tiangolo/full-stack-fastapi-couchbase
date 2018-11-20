fba9acedb9950cd4b601884590e6b9617c2d0741
Sebastián Ramírez <tiangolo@gmail.com>
2018-11-20 16:53:36 +0400

    :sparkles: Add Frontend API password recovery
    and refactor structure

--- {{cookiecutter.project_slug}}/frontend/src/api.ts
+++ {{cookiecutter.project_slug}}/frontend/src/api.ts
@@ -2,67 +2,46 @@ import axios from 'axios';
 import { apiUrl } from '@/env';
 import { IUserProfile, IUserProfileUpdate, IUserProfileCreate } from './interfaces';
 
-const logInGetToken = async (username: string, password: string) => {
-  return axios.post(`${apiUrl}/api/v1/login/access-token`, {
-    username,
-    password,
-  });
-};
-
-const getMe = async (token: string) => {
-  return axios.get<IUserProfile>(`${apiUrl}/api/v1/users/me`, {
+function authHeaders(token: string) {
+  return {
     headers: {
       Authorization: `Bearer ${token}`,
     },
-  });
-};
-
-const updateMe = async (token: string, data: IUserProfileUpdate) => {
-  return axios.put<IUserProfile>(`${apiUrl}/api/v1/users/me`, data, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-};
-
-const getUsers = async (token: string) => {
-  return axios.get<IUserProfile[]>(`${apiUrl}/api/v1/users/`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-};
-
-const updateUser = async (token: string, name: string, data: IUserProfileUpdate) => {
-  return axios.put(`${apiUrl}/api/v1/users/${name}`, data, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-};
-
-const createUser = async (token: string, data: IUserProfileCreate) => {
-  return axios.post(`${apiUrl}/api/v1/users/`, data, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-};
-
-const getRoles = async (token: string) => {
-  return axios.get(`${apiUrl}/api/v1/roles/`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-};
+  };
+}
 
 export const api = {
-  logInGetToken,
-  getMe,
-  updateMe,
-  getUsers,
-  updateUser,
-  createUser,
-  getRoles,
+  async logInGetToken(username: string, password: string) {
+    return axios.post(`${apiUrl}/api/v1/login/access-token`, {
+      username,
+      password,
+    });
+  },
+  async getMe(token: string) {
+    return axios.get<IUserProfile>(`${apiUrl}/api/v1/users/me`, authHeaders(token));
+  },
+  async updateMe(token: string, data: IUserProfileUpdate) {
+    return axios.put<IUserProfile>(`${apiUrl}/api/v1/users/me`, data, authHeaders(token));
+  },
+  async getUsers(token: string) {
+    return axios.get<IUserProfile[]>(`${apiUrl}/api/v1/users/`, authHeaders(token));
+  },
+  async updateUser(token: string, name: string, data: IUserProfileUpdate) {
+    return axios.put(`${apiUrl}/api/v1/users/${name}`, data, authHeaders(token));
+  },
+  async createUser(token: string, data: IUserProfileCreate) {
+    return axios.post(`${apiUrl}/api/v1/users/`, data, authHeaders(token));
+  },
+  async getRoles(token: string) {
+    return axios.get(`${apiUrl}/api/v1/roles/`, authHeaders(token));
+  },
+  async passwordRecovery(username: string) {
+    return axios.post(`${apiUrl}/api/v1/password-recovery/${username}`);
+  },
+  async resetPassword(password: string, token: string) {
+    return axios.post(`${apiUrl}/api/v1/reset-password/`, {
+      new_password: password,
+      token,
+    });
+  },
 };
