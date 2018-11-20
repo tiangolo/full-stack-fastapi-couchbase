a48e8a56d931da863af573117875837b96154a50
Sebastián Ramírez <tiangolo@gmail.com>
2018-11-20 16:49:04 +0400

    :sparkles: Add endpoints for password recovery
    to trigger password recovery and to read the token sent to the email

--- {{cookiecutter.project_slug}}/backend/app/app/api/api_v1/endpoints/token.py
+++ {{cookiecutter.project_slug}}/backend/app/app/api/api_v1/endpoints/token.py
@@ -9,14 +9,17 @@ from webargs import fields
 
 # Import app code
 from app.main import app
-from app.db.bucket import bucket
+from app.db.database import get_default_bucket
 from ..api_docs import docs, security_params
 from app.core import config
-from app.crud.user import authenticate_user, check_if_user_is_active
+from app.crud.user import authenticate_user, check_if_user_is_active, check_if_user_is_superuser, get_user, update_user
+from app.utils import send_reset_password_email, generate_password_reset_token, verify_password_reset_token
+from app.models.user import UserInUpdate
 
 # Import Schemas
 from app.schemas.token import TokenSchema
 from app.schemas.user import UserSchema
+from app.schemas.msg import MsgSchema
 
 
 @docs.register
@@ -30,6 +33,7 @@ from app.schemas.user import UserSchema
 )
 @marshal_with(TokenSchema())
 def route_login_access_token(username, password):
+    bucket = get_default_bucket()
     user = authenticate_user(bucket, username, password)
     if not user:
         abort(400, "Incorrect email or password")
@@ -84,3 +88,43 @@ def route_manual_test_token(test):
     else:
         abort(400, "No user")
     return current_user
+
+
+@docs.register
+@doc(description="Recover password", tags=["login"])
+@app.route(f"{config.API_V1_STR}/password-recovery/<name>", methods=["POST"])
+@marshal_with(MsgSchema())
+def route_recover_password(name):
+    bucket = get_default_bucket()
+    user = get_user(bucket, name)
+
+    if not user:
+        return abort(404, f"The user with this username does not exist in the system.")
+    password_reset_token = generate_password_reset_token(name)
+    send_reset_password_email(email_to=user.email, username=name, token=password_reset_token)
+    return {"msg": "Password recovery email sent"}
+
+
+@docs.register
+@doc(description="Reset password", tags=["login"])
+@app.route(f"{config.API_V1_STR}/reset-password/", methods=["POST"])
+@use_kwargs({
+    "token": fields.Str(required=True),
+    "new_password": fields.Str(required=True)})
+@marshal_with(MsgSchema())
+def route_reset_password(token, new_password):
+    name = verify_password_reset_token(token)
+    if not name:
+        abort(400, "Invalid token")
+    bucket = get_default_bucket()
+    user = get_user(bucket, name)
+    if not user:
+        return abort(404, f"The user with this username does not exist in the system.")
+    elif not check_if_user_is_active(user):
+        abort(400, "Inactive user")
+    user_in = UserInUpdate(
+        name=name,
+        password=new_password,
+    )
+    user = update_user(bucket, user_in)
+    return {"msg": "Password updated successfully"}
