b488ff241c822c5d71a229b322ed7f14b5c85e0b
Sebastián Ramírez <tiangolo@gmail.com>
2018-11-20 16:51:25 +0400

    :art: Update formatting of frontend password edit

--- {{cookiecutter.project_slug}}/frontend/src/views/main/profile/UserProfileEditPassword.vue
+++ {{cookiecutter.project_slug}}/frontend/src/views/main/profile/UserProfileEditPassword.vue
@@ -3,9 +3,7 @@
     <v-card class="ma-3 pa-3">
       <v-card-title primary-title>
         <div class="headline primary--text">Set Password</div>
-
       </v-card-title>
-
       <v-card-text>
         <template>
           <div class="my-3">
@@ -36,11 +34,9 @@
               v-model="password2"
               :error-messages="errors.first('password_confirmation')">
             </v-text-field>
-            <v-btn @click="submit" :disabled="!valid">
-              Save
-            </v-btn>
-            <v-btn @click="reset">Reset</v-btn>
             <v-btn @click="cancel">Cancel</v-btn>
+            <v-btn @click="reset">Reset</v-btn>
+            <v-btn @click="submit" :disabled="!valid">Save</v-btn>
           </v-form>
         </template>
       </v-card-text>
