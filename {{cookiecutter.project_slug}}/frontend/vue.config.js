6b18f83b5ed5e868eea3a793e249d150705efc4d
Sebastián Ramírez <tiangolo@gmail.com>
2018-11-20 16:50:22 +0400

    :bug: Fix vuex-typescript built for prod
    don't rename functions

--- {{cookiecutter.project_slug}}/frontend/vue.config.js
+++ {{cookiecutter.project_slug}}/frontend/vue.config.js
@@ -1,4 +1,23 @@
 module.exports = {
+  // Fix Vuex-typescript in prod: https://github.com/istrib/vuex-typescript/issues/13#issuecomment-409869231
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.optimization.minimizer[0].options.uglifyOptions = Object.assign(
+        {},
+        config.optimization.minimizer[0].options.uglifyOptions,
+        {
+          ecma: 5,
+          compress: {
+            keep_fnames: true,
+          },
+          warnings: false,
+          mangle: {
+            keep_fnames: true,
+          },
+        },
+      );
+    }
+  },
   chainWebpack: config => {
     config.module
       .rule('vue')
