ef6e0fefa7049c122f1da1efeef9bf57085fdf3f
Sebastián Ramírez <tiangolo@gmail.com>
2018-11-20 16:32:36 +0400

    :wrench: Read env vars configs for SMTP and host for links

--- {{cookiecutter.project_slug}}/backend/app/app/core/config.py
+++ {{cookiecutter.project_slug}}/backend/app/app/core/config.py
@@ -18,6 +18,7 @@ if not SECRET_KEY:
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 8  # 60 minutes * 24 hours * 8 days = 8 days
 
 SERVER_NAME = os.getenv("SERVER_NAME")
+SERVER_HOST = os.getenv("SERVER_HOST")
 BACKEND_CORS_ORIGINS = os.getenv(
     "BACKEND_CORS_ORIGINS"
 )  # a string of origins separated by commas, e.g: "http://localhost, http://localhost:4200, http://localhost:3000, http://localhost:8080, http://dev.couchbase-project.com, https://stag.couchbase-project.com, https://couchbase-project.com, http://local.dockertoolbox.tiangolo.com"
@@ -46,6 +47,19 @@ COUCHBASE_SYNC_GATEWAY_DATABASE = os.getenv("COUCHBASE_SYNC_GATEWAY_DATABASE")
 COUCHBASE_CORS_ORIGINS = os.getenv("COUCHBASE_CORS_ORIGINS")
 # a string of origins separated by commas, e.g: "http://localhost:5984, http://localhost, http://localhost:4200, http://localhost:3000, http://localhost:8080, http://dev.couchbase-project.com, https://stag.couchbase-project.com, https://db.stag.couchbase-project.com, https://couchbase-project.com, https://db.couchbase-project.com, http://local.dockertoolbox.tiangolo.com, http://local.dockertoolbox.tiangolo.com:5984"
 COUCHBASE_AUTH_TIMEOUT = ACCESS_TOKEN_EXPIRE_MINUTES * 60
+
+SMTP_TLS = getenv_boolean("SMTP_TLS", True)
+SMTP_PORT = None
+_SMTP_PORT = os.getenv("SMTP_PORT")
+if _SMTP_PORT is not None:
+    SMTP_PORT = int(_SMTP_PORT)
+SMTP_HOST = os.getenv("SMTP_HOST")
+SMTP_USER = os.getenv("SMTP_USER")
+SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
+EMAILS_FROM_EMAIL = os.getenv("EMAILS_FROM_EMAIL")
+EMAILS_FROM_NAME = PROJECT_NAME
+EMAIL_RESET_TOKEN_EXPIRE_HOURS=48
+EMAIL_TEMPLATES_DIR="/app/app/email-templates/build"
 
 ROLE_SUPERUSER = "superuser"
 
