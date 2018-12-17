29a71e69a29d3b366da8ca7cce289ba2e13307ea
Sebastián Ramírez <tiangolo@gmail.com>
2018-12-17 13:57:12 +0400

    :bug: Fix timeout types in bucket creation

--- {{cookiecutter.project_slug}}/backend/app/app/db/database.py
+++ {{cookiecutter.project_slug}}/backend/app/app/db/database.py
@@ -41,8 +41,8 @@ def get_bucket(
     bucket_name: str,
     host="couchbase",
     port="8091",
-    timeout: int = COUCHBASE_OPERATION_TIMEOUT_SECS,
-    n1ql_timeout: int = COUCHBASE_N1QL_TIMEOUT_SECS,
+    timeout: float = COUCHBASE_OPERATION_TIMEOUT_SECS,
+    n1ql_timeout: float = COUCHBASE_N1QL_TIMEOUT_SECS,
 ):
     cluster = get_cluster(username, password, host=host, port=port)
     bucket: Bucket = cluster.open_bucket(bucket_name, lockmode=LOCKMODE_WAIT)
