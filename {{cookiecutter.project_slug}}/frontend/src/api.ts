aa74e3d397b4cd3033afe3413526bf5e12f0ae39
Sebastián Ramírez <tiangolo@gmail.com>
2019-01-10 20:11:40 +0400

    :bug: Fix frontend login using form (required by OAuth2)
     #1 #2

--- {{cookiecutter.project_slug}}/frontend/src/api.ts
+++ {{cookiecutter.project_slug}}/frontend/src/api.ts
@@ -12,10 +12,11 @@ function authHeaders(token: string) {
 
 export const api = {
   async logInGetToken(username: string, password: string) {
-    return axios.post(`${apiUrl}/api/v1/login/access-token`, {
-      username,
-      password,
-    });
+    const params = new URLSearchParams();
+    params.append('username', username);
+    params.append('password', password);
+
+    return axios.post(`${apiUrl}/api/v1/login/access-token`, params);
   },
   async getMe(token: string) {
     return axios.get<IUserProfile>(`${apiUrl}/api/v1/users/me`, authHeaders(token));
