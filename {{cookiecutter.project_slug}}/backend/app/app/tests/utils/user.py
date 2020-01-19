de806789211dc712448bfa8c9b537eb939892ce5
Sebastián Ramírez <tiangolo@gmail.com>
2020-01-19 13:28:13 +0100

    :sparkles: Add normal-user testing fixture (#23)

--- {{cookiecutter.project_slug}}/backend/app/app/tests/utils/user.py
+++ {{cookiecutter.project_slug}}/backend/app/app/tests/utils/user.py
@@ -3,8 +3,8 @@ import requests
 from app import crud
 from app.core import config
 from app.db.database import get_default_bucket
-from app.models.user import UserCreate
-from app.tests.utils.utils import random_lower_string
+from app.models.user import UserCreate, UserUpdate
+from app.tests.utils.utils import random_lower_string, get_server_api
 
 
 def user_authentication_headers(server_api, email, password):
@@ -24,3 +24,23 @@ def create_random_user():
     bucket = get_default_bucket()
     user = crud.user.upsert(bucket, user_in=user_in, persist_to=1)
     return user
+
+
+def authentication_token_from_email(email):
+    """
+    Return a valid token for the user with given email.
+
+    If the user doesn't exist it is created first.
+    """
+    password = random_lower_string()
+    bucket = get_default_bucket()
+
+    user = crud.user.get_by_email(bucket, email=email)
+    if not user:
+        user_in = UserCreate(username=email, email=email, password=password)
+        user = crud.user.upsert(bucket, user_in=user_in, persist_to=1)
+    else:
+        user_in = UserUpdate(password=password)
+        user = crud.user.update(bucket, user=user, user_in=user_in)
+
+    return user_authentication_headers(get_server_api(), email, password)
